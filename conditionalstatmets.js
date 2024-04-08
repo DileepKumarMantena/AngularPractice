df62d611b527dbf8bec394415982b861ceda5da6
DileepKumarMantena <dileep@nuevesolutions.com>
2024-04-08 21:27:24 +0530

    Added new tasks

---
+++ conditionalstatmets.js
@@ -0,0 +1,87 @@
+// let marks = 36;
+
+// if (marks > 90) {
+//     console.log("you got a grade");
+//  }
+//  else if (marks >80) {
+//     console.log("you got B grade");
+//  }
+//  else if (marks >70) {
+//     console.log("you got C grade");
+//  }
+// else {
+//     console.log("YOU GOT F GRADE");
+// }
+
+// // number =8
+
+// // if (number >0){
+// //     console.log("postive number")
+// // }
+// // else {
+// //     console.log('negitive number ')
+
+// // }
+
+
+// // a=7
+
+// // // a/2
+// // // a % 2 
+
+// // if (a%2==0){
+// //     console.log('even number')
+// // }
+// // else if (a %2!=0){
+// //     console.log('odd number')
+// // }
+// // else {
+// //     console.log('number is 0')
+// // }
+
+
+// b= 3
+// if (b > 0){
+//     if (b%2==0){
+//         console.log('even number')
+//     }
+//     else {
+//         console.log('odd number ')
+//     }
+// }
+// else {
+//     console.log('it is not a number')
+// }
+
+
+// age = 18
+// if (a>=18){
+//     console.log('  Eligble to vote ')
+// }
+// else{
+//     console.log('not eligible to vote ')
+// } 
+
+// let day = "Friday";
+
+// switch (day) {
+//     case "Monday":
+//         console.log("It's the start of the week.");
+//         break;
+//     case "Friday":
+//         console.log("It's almost the weekend!");
+//         break;
+//     default:
+//         console.log("It's a regular day.");
+// }
+
+
+for (let i = 1; i <= 5; i++) {
+    if (i%2==0){
+        console.log('even',i)
+    }
+    else{
+        console.log('odd',i)
+    }
+  }
+  
