b476f2505d634f4ef52dba8f24faf1b8b283299e
santhosh gadde <santhugadde908@gmail.com>
2024-04-11 09:16:16 -0500

    functions test

--- conditionalstatmets.js
+++ conditionalstatmets.js
@@ -99,3 +99,8 @@ number=2
 for (let i = 1; i <= 5; i++){
     console.log(i*2)
 }
+
+
+for (let i = 1; i <= 10; i++){
+    console.log(i*9)
+}
--- functions.js
+++ functions.js
@@ -1,11 +1,24 @@
 function greet(name) {
-    console.log(`Hello, ${name}!`);
-  }
-  
-greet("John"); 
-  
+  console.log(`Hello, ${name}!`);
+}
+
+greet("John");
+
 const greet = (name) => {
-    console.log(`Hello, ${name}!`);
-  };
-  
+  console.log(`Hello, ${name}!`);
+};
+
 greet("Deepak")
+
+
+function evenOrOdd(number) {
+  if (number % 2 == 0) {
+    return "evenNumber";
+  }
+  else {
+    return "oddNumber";
+  }
+}
+
+console.log(evenOrOdd(5)); 
+console.log(evenOrOdd(10));
