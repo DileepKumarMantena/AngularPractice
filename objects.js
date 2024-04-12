f29244a64fab67930c9d8580e9804f52e677989e
DileepKumarMantena <dileep@nuevesolutions.com>
2024-04-12 21:02:48 +0530

    String Methods

--- objects.js
+++ objects.js
@@ -1,32 +1,58 @@
-// const person = {
-//     firstName: 'John',
-//     lastName: 'Doe',
-//     age: 25,
-// }  
+// // // const person = {
+// // //     firstName: 'John',
+// // //     lastName: 'Doe',
+// // //     age: 25,
+// // // }  
 
-//hetrogenus data type    bird
-// console.log(person["age"])
-// person['age']=85
-// console.log(person)
-// console.log(Object.keys(person))
-// console.log(Object.values(person))
+// // //hetrogenus data type    bird
+// // // console.log(person["age"])
+// // // person['age']=85
+// // // console.log(person)
+// // // console.log(Object.keys(person))
+// // // console.log(Object.values(person))
 
-// const { firstName, lastName } = person;
-// console.log(firstName, lastName);
+// // // const { firstName, lastName } = person;
+// // // console.log(firstName, lastName);
 
-// const currentDate = new Date();
+// // // const currentDate = new Date();
 
-// console.log(currentDate.getFullYear())
+// // // console.log(currentDate.getFullYear())
 
-function Car(make, model, year) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
-    this.displayInfo = function() {
-      console.log(`${this.year} ${this.make} ${this.model}`);
-    };
-  }
-const myCar = new Car('Toyota', 'Camry', 2022);
-myCar.displayInfo(); 
+// // // function Car(make, model, year) {
+// // //     this.make = make;
+// // //     this.model = model;
+// // //     this.year = year;
+// // //     this.displayInfo = function() {
+// // //       console.log(`${this.year} ${this.make} ${this.model}`);
+// // //     };
+// // //   }
+// // // const myCar = new Car('Toyota', 'Camry', 2022);
+// // // myCar.displayInfo(); 
   
+
+// // let target = { a: 1 };
+// // let source1 = { b: 2 };
+// // let source2 = { c: 3, d: 4 };
+
+// // Object.assign(source1, target, source2);
+// // console.log(target);
+// // console.log(source1)
+
+
+// let obj = { a: 1, b: 2, c: 3 };
+// console.log(Object.entries(obj));
+// console.log(Object.keys(obj))
+// console.log(Object.values(obj))
+
+
+a=[1,2,3]
+let obj = { a: 1, b: 2 };
+Object.freeze(obj);
+obj.b = 3; 
+console.log(obj.b);
+
+b={
+  "name":['Deepak',"JavaScript"]
+}
+
 
